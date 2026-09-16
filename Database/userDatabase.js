94ab74b97b2ec083bbf434584ba59a173038c9bc
Kuldeep <ksinda05@gmail.com>
2026-09-16 09:35:10 +0530

    done

--- Database/userDatabase.js
+++ Database/userDatabase.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 
-const pathToFile = path.join(__dirname, "../../users.json");
+const pathToFile = path.join(__dirname, "../users.json");
 
 
 const getUsersFromDatabase = (callback) => {
